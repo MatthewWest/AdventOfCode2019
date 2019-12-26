fc97c4ec07bbcb955e2f17d11a324f428e3ab2a9
Matt West <matthew.a.west.17@dartmouth.edu>
2019-12-26 11:48:48 -0700

    Day 18

--- day18.py
+++ day18.py
@@ -3,6 +3,9 @@ from utils import defaultdict_repr, find_coordinate_bounds
 from collections import deque
 import re
 from copy import copy
+from queue import PriorityQueue
+from dataclasses import dataclass, field
+from typing import Any
 
 
 def parse_input(s):
@@ -43,192 +46,203 @@ def is_door(c):
 def print_map(m, pos):
   m = copy(m)
   m[pos] = '@'
-  print(defaultdict_repr(m))  
+  print(defaultdict_repr(m))
 
 
-def resolve_all_wall_deps_to_keys(all_deps):
-  all_new_deps = {}
-  for k in all_deps:
-    if all_deps.get(k) is None:
-      all_new_deps[k] = None
-      continue
-    deps = set(all_deps.get(k))
-    new_deps = set()    
-    while True:
-      for dep in deps:
-        if is_key(dep):
-          new_deps.add(dep)
-        else:
-          new_deps.update(all_deps.get(dep))
-      if new_deps == deps:
-        break
-      deps = copy(new_deps)
-    all_new_deps[k] = [k for k in new_deps if is_key(k)]
-  all_new_deps = {k: all_new_deps[k] for k in all_new_deps if is_key(k)} 
-  return all_new_deps
-
-
-def find_dependencies(m, pos):
-  visited = set()
-  to_visit = deque()
-  to_visit.append((pos, None))  # coordinate + the immediate blocking dependency
-  blocked_by = defaultdict(lambda: [])
-  while to_visit:
-    p, blockers = to_visit.popleft()
-    c = m[p]
-    if is_key(c):
-      if blockers:
-        for blocker in blockers:
-          blocked_by[c].append(blocker)
-      else:
-        blocked_by[c] = None
-    elif is_door(c):
-      blocked_by[c].append(c.lower())
-      if blockers:
-        for blocker in blockers:
-          blocked_by[c].append(blocker)
-      blockers = [c]
-    for n in get_neighbors(m, p):
-      if n not in visited:
-        to_visit.append((n, blockers))
-    visited.add(p)
-
-  return resolve_all_wall_deps_to_keys(blocked_by)
-
-
-def makes_available(dependencies):
-  is_depended_on = defaultdict(lambda: [])
-  for c in dependencies:
-    deps = dependencies[c]
-    if deps is None:
-      continue
-    for dep in deps:
-      is_depended_on[dep].append(c)
-  return is_depended_on
-
-
-def find_keys(m):
-  locs = {}
-  min_x, max_x, min_y, max_y = find_coordinate_bounds(m)
-  for x in range(min_x, max_x+1):
-    for y in range(min_y, max_y+1):
-      c = m[(x, y)]
-      if is_key(c):
-        locs[c] = (x,y)
-  return locs
-
-
-def find_path_not_crossing_other_key(m, start, dest):
+def find_path(m, start, dest):
   visited = set()
   to_visit = deque()
   to_visit.append(start)
   paths = {start: []}
-  keys_needed = defaultdict(lambda: set())
+  needed_keys = defaultdict(lambda: set())
   while to_visit:
     p = to_visit.popleft()
-    if p == dest:
-      return paths[p], keys_needed[p]
+    visited.add(p)    
     c = m[p]
-    # if is_key(c) and p != start and p != dest:
-    #   continue
     if is_door(c):
-      keys_needed[p].add(c.lower())
+      needed_keys[p].add(c.lower())
     for n in get_neighbors(m, p):
       if n not in visited:
         to_visit.append(n)
         paths[n] = paths[p] + [n]
-        keys_needed[n] = copy(keys_needed[p])
-    visited.add(p)
+        needed_keys[n] = copy(needed_keys[p])
 
   if dest in paths:
-    return paths[dest], keys_needed[dest]
+    return paths[dest], needed_keys[dest]
   else:
     return None, None
 
 
-def find_initial_candidates(deps):
-  candidates = []
-  for c in deps:
-    if deps[c] is None:
-      candidates.append(c)
-  return candidates
+def find_points_of_interest(m):
+  locs = set()
+  min_x, max_x, min_y, max_y = find_coordinate_bounds(m)
+  for x in range(min_x, max_x+1):
+    for y in range(min_y, max_y+1):
+      if is_key(m[(x, y)]):
+        locs.add((x,y))
+  return locs
 
-
-def find_all_paths(m, pos):
-  keys = find_keys(m)
+def find_paths_and_needed_keys(m, start):
+  locs = list(find_points_of_interest(m)) + [start]
   paths = defaultdict(lambda: {})
-  deps = defaultdict(lambda: {})
-  for k in keys:
-    p, needed = find_path_not_crossing_other_key(m, pos, keys[k])
-    if p is not None and needed is not None:
-      paths['@'][k], deps['@'][k] = p, needed
-  for a in keys:
-    for b in keys:
-      if b == a:
+  for a in locs:
+    for b in locs:
+      # Don't navigate to ourselves or to the start or anywhere to start
+      if a is b or b is start:
         continue
-      p, needed = find_path_not_crossing_other_key(m, keys[a], keys[b])
-      if p is not None and needed is not None:
-        paths[a][b], deps[a][b] = p, needed
-  return paths, deps
+      path, keys_needed = find_path(m, a, b)
+      paths[a][b] = (path, keys_needed)
+  return paths
 
 
-def get_candidates(start, available, deps):
-  candidates = []
-  for dest in deps[start]:
+class Node:
+  def __init__(self, loc, keys):
+    self._loc = loc
+    self._keys = frozenset(list(keys))
 
-    if deps[start][dest] <= available:
-      candidates.append(dest)
-  return candidates
+  @property
+  def loc(self):
+    return self._loc
+
+  @property
+  def keys(self):
+    return self._keys
+  
+  def __key(self):
+    return (self._loc, self._keys)
+
+  def __hash__(self):
+    return hash(self.__key())
+
+  def __eq__(self, other):
+    return self.__key() == other.__key()
+
+  def __lt__(self, other):
+    return self.keys < other.keys
+
+  def __repr__(self):
+    return 'Node<loc={:}, keys={:}>'.format(self._loc, set(self._keys))
 
 
-def find_shortest_path_to_all_keys(m, start):
-  locs = find_locs(m)
-  deps = find_dependencies(m, start)
-  candidates = set(find_initial_candidates(deps))
-  reverse_deps = makes_available(deps)
-  return find_shortest_path(m, start, candidates, set(), locs, deps, reverse_deps)
-
-
-def find_shortest_path(m, 
-                       start, 
-                       candidates,
-                       can_open,
-                       locs,
-                       deps,
-                       downstreams):
-  # Base case, if we have no more keys to find, return
-  if len(candidates) == 0:
-    return []
-
-  shortest_path = None
-  shortest_length = float('inf')
-  for candidate in candidates:
-    path = find_path(m, start, locs[candidate], can_open)
-    if path is None:
+def find_node_neighbors(m, node, paths):
+  pos, keys = node.loc, node.keys
+  visited = set()
+  to_visit = deque()
+  paths_from_pos = paths[pos]
+  node_neighbors = []
+  for dest in paths_from_pos:
+    if m[dest] in keys:
       continue
+    path, keys_needed = paths_from_pos[dest]
+    if keys_needed <= keys:
+      node_neighbors.append((len(path), Node(dest, list(keys) + [m[dest]])))
+  return node_neighbors
 
-    now_can_open = copy(can_open)
-    now_can_open.add(candidate.upper())
 
-    new_candidates = set()
-    for c in candidates | set(downstreams[candidate]):
-      if c == candidate:
-        continue
-      new_candidates.add(c)
+from heapq import heapify, heappush, heappop
 
-    rest = find_shortest_path(m,
-      locs[candidate],
-      new_candidates,
-      now_can_open,
-      locs,
-      deps,
-      downstreams)
+class PriorityDict(dict):
+  """Dictionary that can be used as a priority queue.
 
-    path = path + rest
-    if len(path) < shortest_length:
-      shortest_path = path
-      shortest_length = len(path)
-  return shortest_path
+  Keys of the dictionary are items to be put into the queue, and values
+  are their respective priorities. All dictionary methods work as expected.
+  The advantage over a standard heapq-based priority queue is
+  that priorities of items can be efficiently updated (amortized O(1))
+  using code as 'thedict[item] = new_priority.'
+
+  The 'smallest' method can be used to return the object with lowest
+  priority, and 'pop_smallest' also removes it.
+
+  The 'sorted_iter' method provides a destructive sorted iterator.
+  """
+  
+  def __init__(self, *args, **kwargs):
+    super(PriorityDict, self).__init__(*args, **kwargs)
+    self._rebuild_heap()
+
+  def _rebuild_heap(self):
+    self._heap = [(v, k) for k, v in self.items()]
+    heapify(self._heap)
+
+  def smallest(self):
+    """Return the item with the lowest priority.
+
+    Raises IndexError if the object is empty.
+    """
+    
+    heap = self._heap
+    v, k = heap[0]
+    while k not in self or self[k] != v:
+      heappop(heap)
+      v, k = heap[0]
+    return k
+
+  def pop_smallest(self):
+    """Return the item with the lowest priority and remove it.
+
+    Raises IndexError if the object is empty.
+    """
+    
+    heap = self._heap
+    v, k = heappop(heap)
+    while k not in self or self[k] != v:
+      v, k = heappop(heap)
+    del self[k]
+    return k
+
+  def __setitem__(self, key, val):
+    # We are not going to remove the previous value from the heap,
+    # since this would have a cost O(n).
+    
+    super(PriorityDict, self).__setitem__(key, val)
+    
+    if len(self._heap) < 2 * len(self):
+      heappush(self._heap, (val, key))
+    else:
+      # When the heap grows larger than 2 * len(self), we rebuild it
+      # from scratch to avoid wasting too much memory.
+      self._rebuild_heap()
+
+  def setdefault(self, key, val):
+    if key not in self:
+      self[key] = val
+      return val
+    return self[key]
+
+  def update(self, *args, **kwargs):
+    # Reimplementing dict.update is tricky -- see e.g.
+    # http://mail.python.org/pipermail/python-ideas/2007-May/000744.html
+    # We just rebuild the heap from scratch after passing to super.
+    
+    super(PriorityDict, self).update(*args, **kwargs)
+    self._rebuild_heap()
+
+  def sorted_iter(self):
+    """Sorted iterator of the priority dictionary items.
+
+    Beware: this will destroy elements as they are returned.
+    """
+    while self:
+      yield self.pop_smallest()
+
+def djikstra(m, pos, paths):
+  pq = PriorityDict()
+  dist = defaultdict(lambda: float('inf'))
+  prev = {}
+  start = Node(pos, [])
+  # Start with the current position and an empty set of keys at distance 0
+  pq[start] = 0
+  dist[start] = 0
+  while pq:
+    node = pq.pop_smallest()
+    for length_to_neighbor, neighbor in find_node_neighbors(m, node, paths):
+      alt_dist = dist[node] + length_to_neighbor
+      if alt_dist < dist[neighbor]:
+        dist[neighbor] = alt_dist
+        prev[neighbor] = node
+        pq[neighbor] = alt_dist
+  return dist, prev
+
 
 
 s1 = '''#########
@@ -265,39 +279,144 @@ s5 = '''########################
 ###g#h#i################
 ########################'''
 
-m, pos = parse_input(s4)
-# m, pos = get_input()
 
-paths, deps = find_all_paths(m, pos)
-keys = find_keys(m).keys()
-visited = set()
-available = set()
-at = '@'
-candidates = get_candidates(at, available, deps)
+def part1():
+  m, pos = get_input()
 
-def find_shortest_path_to_all_keys(m, at, candidates, available):
-  # print(at, candidates, available)
-  s_len = float('inf')
-  s_path = None
-  for candidate in candidates:
-    if candidate in available:
+  paths = find_paths_and_needed_keys(m, pos)
+  dist, prev = djikstra(m, pos, paths)
+
+  key_count = 0
+  end = None
+  for node in dist:
+    if len(node.keys) > key_count:
+      key_count = len(node.keys)
+
+  least_distance = float('inf')
+  for node in dist:
+    if len(node.keys) != key_count:
       continue
+    if dist[node] < least_distance:
+      least_distance = dist[node]
 
-    now_available = available | set([candidate])
-    path = paths[at][candidate]
-    rest = find_shortest_path_to_all_keys(m,
-      candidate,
-      get_candidates(candidate, now_available, deps),
-      now_available)
-    path = path + rest
-    if len(path) < s_len:
-      s_len = len(path)
-      s_path = path
-  if s_path is None:
-    return []
-  else:
-    return s_path
+  return least_distance
 
 
-path = find_shortest_path_to_all_keys(m, '@', candidates, set())
-print(len(path))
+class Node2:
+  def __init__(self, robot_locs, keys):
+    self._locs = frozenset(list(robot_locs))
+    self._keys = frozenset(list(keys))
+
+  @property
+  def locs(self):
+    return self._locs
+
+  @property
+  def keys(self):
+    return self._keys
+  
+  def __key(self):
+    return (self._locs, self._keys)
+
+  def __hash__(self):
+    return hash(self.__key())
+
+  def __eq__(self, other):
+    return self.__key() == other.__key()
+
+  def __lt__(self, other):
+    return self.keys < other.keys
+
+  def __repr__(self):
+    return 'Node<locs={:}, keys={:}>'.format(self.locs, set(self.keys))
+
+
+
+def correct_map_and_get_locs(m, pos):
+  x, y = pos
+  m[(x, y)] = '#'
+  m[(x-1, y)] = '#'
+  m[(x+1, y)] = '#'
+  m[(x, y-1)] = '#'
+  m[(x, y+1)] = '#'
+  locs = [(x+1, y+1), (x+1, y-1), (x-1, y+1), (x-1, y-1)]
+  return locs
+
+
+def find_node_neighbors2(m, node, paths):
+  locs, keys = node.locs, node.keys
+  visited = set()
+  node_neighbors = []  
+  for loc in locs:
+    for dest in paths[loc]:
+      if m[dest] in keys:
+        continue
+      path, keys_needed = paths[loc][dest]
+      if not path:  # no path
+        raise Exception
+      if keys_needed <= keys:
+        new_locs = set(locs)
+        new_locs.remove(loc)
+        new_locs.add(dest)
+        node_neighbors.append((len(path), Node2(new_locs, list(keys) + [m[dest]])))
+  return node_neighbors
+
+
+def djikstra2(m, locs, paths):
+  pq = PriorityDict()
+  steps = defaultdict(lambda: float('inf'))
+  prev = {}
+  start = Node2(locs, [])
+  # Start with the current positions and an empty set of keys at distance 0
+  pq[start] = 0
+  steps[start] = 0
+  while pq:
+    node = pq.pop_smallest()
+    for length_to_neighbor, neighbor in find_node_neighbors2(m, node, paths):
+      alt_steps = steps[node] + length_to_neighbor
+      if alt_steps < steps[neighbor]:
+        steps[neighbor] = alt_steps
+        prev[neighbor] = node
+        pq[neighbor] = alt_steps
+  return steps, prev
+
+def find_paths_and_needed_keys2(m, locs):
+  locs = set(locs)
+  to_find = list(find_points_of_interest(m)) + list(locs)
+  paths = defaultdict(lambda: {})
+  for a in to_find:
+    for b in to_find:
+      # Don't navigate to ourselves or to the start or anywhere to start
+      if a is b or b in locs:
+        continue
+      path, keys_needed = find_path(m, a, b)
+      if path:
+        paths[a][b] = (path, keys_needed)
+  return paths
+
+
+def part2():
+  m, pos = get_input()
+  locs = correct_map_and_get_locs(m, pos)
+
+  paths = find_paths_and_needed_keys2(m, locs)
+  dist, prev = djikstra2(m, locs, paths)
+
+  key_count = 0
+  end = None
+  for node in dist:
+    if len(node.keys) > key_count:
+      key_count = len(node.keys)
+
+  least_distance = float('inf')
+  for node in dist:
+    if len(node.keys) != key_count:
+      continue
+    if dist[node] < least_distance:
+      least_distance = dist[node]
+
+  return least_distance
+
+
+print(part1())
+print(part2())
